168eb7172296fa77314924234aabcd2c94487a0a
Badr <moslihbadr2@gmail.com>
2023-05-31 23:52:40 +0100

    Handle form submission and insert form data into the database

--- new_blog.php
+++ new_blog.php
@@ -1,4 +1,5 @@
 <?php include("./php/connect_DB.php");?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,18 +15,18 @@
 <?php include("./php/header.php");?>
 <br><br>
   <div class="container d-flex justify-content-center flex-column my-5 w-50 w-md-100">
-      <form action="">
+      <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
       <div class="mb-3">
         <label for="exampleFormControlInput1" class="form-label">Your Name :</label>
-        <input type="text" class="form-control" id="exampleFormControlInput1">
+        <input type="text" class="form-control" id="exampleFormControlInput1" name="blog_writer">
       </div>
       <div class="mb-3">
         <label for="exampleFormControlInput1" class="form-label">Blog Title :</label>
-        <input type="text" class="form-control" id="exampleFormControlInput1">
+        <input type="text" class="form-control" id="exampleFormControlInput1" name="blog_title">
       </div>
       <div class="mb-3">
         <label for="exampleFormControlTextarea1" class="form-label">Blog Body :</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="blog_body"></textarea>
       </div>
       <input type="submit" value="Submit" class="btn btn-outline-success">
     </form>
@@ -38,3 +39,27 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.min.js"></script>
 </body>
 </html>
+<?php 
+// date and time format
+function blog_DOC() {
+  $currentDateTime = date("Y-m-d H:i:s");
+  $formattedDateTime = "Posted on " . date("Y-m-d") . " at " . date("h:i A", strtotime($currentDateTime));
+  return $formattedDateTime;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $blog_writer = $_POST['blog_writer'];
+  $blog_title = $_POST['blog_title'];
+  $blog_body = $_POST['blog_body'];
+  
+  // Prepare the query with form data
+  $query = "INSERT INTO blogs (blog_Writer, blog_Title, blog_Body, blog_DOC) VALUES ('$blog_writer', '$blog_title', '$blog_body', ".blog_DOC().");";
+  
+  // Execute the query
+  $result = mysqli_query($conn, $query);
+}
+
+
+
+
+?>
