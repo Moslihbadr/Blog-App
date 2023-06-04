73904490ba4e3c1e698a532679e7f22a9a72c03c
Badr <moslihbadr2@gmail.com>
2023-06-04 23:45:42 +0100

    add header and footer

---
+++ sign_up.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
+  <link rel="stylesheet" href="../Blog/css/bootstrap.min.css"/>
+  <link rel="stylesheet" type="text/css" href="../Blog/css/style.css?"/>
+  <title>Sign up</title>
+</head>
+<body>
+  <?php include("./php/header.php");?>
+  
+  
+
+  <?php include("./php/footer.php");?>
+
+  <script src="../Blog/js/jquery.min.js"></script>
+  <script src="../Blog/js/script.js"></script>
+  <script src="../Blog/js/bootstrap.min.js"></script>
+</body>
+</html>
